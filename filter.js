d596b5be686c723a6ecfe6f870d4df28f5426030
Nikhil208-cloud <nikhilbadoni@NIKHILs-MacBook-Air.local>
2025-01-20 19:29:22 +0530

    my second commit

--- filter.js
+++ filter.js
@@ -1,7 +1,17 @@
-const A=[11,22,33,0,44,55,66,77,88,99,110];
- const B=A.map((i)=>{
-   // return i;
-    return i<66;
-})
-console.log(B);
-console.log(A);
+// const A=[11,22,33,0,44,55,66,77,88,99,110];
+//  const B=A.map((i)=>{
+//    // return i;
+//     return i<66;
+// })
+// console.log(B);
+// console.log(A);
+
+const arr=[1,2,3,4,5]
+
+
+const output=arr.filter((x)=>{
+   //return (x%2 === 0) for even number
+   return x%2
+});
+
+console.log(output);
---
+++ reduce.js
@@ -0,0 +1,39 @@
+// var myArray=[1,2,3,4,5,6];
+// var sum= myArray.reduce(function(acc,curr){
+//      return acc+curr;
+// },0);
+// console.log(sum);
+
+
+const users=[
+    {firstName:"john",lastName:"Biden",age:26},
+    {firstName:"jimmy",lastName:"cob",age:75},
+    {firstName:"sam",lastName:"lewis",age:50},
+    {firstName:"Ronald",lastName:"Mathew",age:26},
+    {firstName:"Ronald1",lastName:"Mathew1",age:26},
+    {firstName:"sam1",lastName:"lewis1",age:50}  
+  ];
+  //using reduce array method
+  const output=users.reduce(function(acc,curr){
+      if(acc[curr.age])
+      //if present in array object
+      {
+          acc[curr.age]++;            
+      }else{
+      //if not present in array object
+        acc[curr.age]=1;      
+      }
+      return acc;
+  },{})
+  console.log(output);
+
+
+
+  const acc ={
+   "26" : 2,
+   "75" :1,
+   "50": 1
+
+  }
+
+  acc[23] = 1
